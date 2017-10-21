38337022071f41261d962f482ee228f4030306a4
gitLinda <lkrueger17@gmail.com>
2017-10-21 15:20:17 +0200

    generating webpack bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
 const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = {
-    entry: './src/app/app.js',
-    output: { filename: './public/bundle.js' },
+    entry: {
+        app: './src/app/app.js',
+        vendor: ['jquery', 'bootstrap', 'lodash', 'less']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: './app/[name].[chunkhash].js' },
     devtool: 'source-map',
+    module: {
+        rules: [
+            {test: /\.css/, use: ExtractTextWebpackPlugin.extract({use: 'css-loader'})},
+            {test: /\.less$/, use: ExtractTextWebpackPlugin.extract({use: ['css-loader', 'less-loader']})
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
-        })
+        }),
+        new HtmlWebpackPlugin({template:'src/index.html'}),
+        new ExtractTextWebpackPlugin('app/app.[chunkhash].css'),
+        new webpack.optimize.CommonsChunkPlugin({names: ['vendor', 'manifest']})
     ]
 }
 
